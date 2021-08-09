5df22800dfd9360a9e89b0115b039d76623f0dc4
Daniel Furman <dryanfurman@gmail.com>
2021-08-09 08:42:23 -0700

    Update _main.py

--- PySDMs/_main.py
+++ PySDMs/_main.py
@@ -106,21 +106,7 @@ class PySDMs(object):
         self.output_dir = 'outputs/'
 
     def fit(self, deep_learning=False, soft_voters=False, tuning=True):
-
-        """Model training with PyCaret pipelines. Constructs soft voters via
-        exhaustive best subset selection among the above BRTs (below mod_list).
-        The function then adds an assortment of neural networks, at two, three,
-        and four hidden layers. The final model is selected from the best
-        validation (F1 score, 30% hold out  default) set and saves the .pkl to
-        file alongside the scores.
-
-        Local fit vars are relatively straightforward True/False that determine
-        which types of models are tried.
-
-        Returns:
-            The voter with the best validation metric performance."""
-
-        # Print a markdown title
+        # ML classification fitting
         display(Markdown('--- \n ### PyCaret Model Fitting: \n --- ' ))
         return(internal_fit(self.data, self.test_data, self.seed, self.target,
             self.exp_name, self.normalize, self.metric, self.fold,
@@ -128,46 +114,12 @@ class PySDMs(object):
             soft_voters, tuning))
 
     def interpolate(self, asc_input_dir, df_input_dir, img_output_dir):
-
-        """Geo-classification function for model interpolation to raster
-        surfaces of the feature variables. Outputs both a probabilistic and
-        binary prediction. See PyImpute's README.md for a very basic intro
-        to geo-classification in Python.
-
-        asc_input_dir, string
-            The directory containing your raster surfaces, called in a manner
-            such that glob can grab all of your training features. For
-            example, if your files are saved as bclim1.asc, bclim2.asc, etc.,
-            then you would include bclim*.asc at the end of the string (e.g.,
-            'data/train-rasters-2.5m/bclim*.asc'').
-
-        df_input_dir, string
-            The input directory for the training dataframe, as called in
-            self.fit() above for the data variable. Should include all of the
-            directory path except for the random seed (end of the name) and
-            the .csv extension (e.g., 'data/env_train/env_train_xv_').
-
-        img_output_dir
-            The output directory for the geo-classification results.
-
-        seed, int
-            The randomstate seed of the modeling run."""
-
+        # Geospatial step for raster predictions
         display(Markdown("--- \n ### Geo-classification Interpolation: \n ---"))
         return(internal_interpolate(asc_input_dir, df_input_dir,
             img_output_dir, self.seed, self.species_name))
 
     def validation_visuals(self, min_seed, max_seed):
-
-        """A function that generates a validation-set F1 boxplot and a AUC
-        ROC analysis plot with CV. The function was developed for visualizing
-        performance across multiple runs between consecutive seed ints.
-
-        min_seed/max_seed: int
-            The min and max seed model runs to grab for the F1 boxplot.
-
-        AUC_seed: int
-            The seed model run to perform CV ROC analysis."""
-
+        # Dataviz for model fitting
         display(Markdown('--- \n ### Model Performance Plots: \n ---'))
         return(internal_validation_visuals(min_seed, max_seed, self.output_dir, self.species_name))
