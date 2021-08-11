b7a026d1b20641410584aea33189eede550aa145
Daniel Furman <dryanfurman@gmail.com>
2021-08-11 14:48:36 -0700

    Update test_operations.py

--- test/test_operations.py
+++ test/test_operations.py
@@ -55,6 +55,6 @@ def test_PySDMs():
 
         # Geo-classification with self.interpolate()
         coastal_redwoods.interpolate(tif_input_dir, df_input_dir, output)
-        coastal_redwoods.validation_visuals(min_seed=seed, max_seed=seed+1)
+        coastal_redwoods.validation_visuals(min_seed=seed, max_seed=seed+1, output)
 
     assert os.path.isfile(output + 'probability_1.tif')
